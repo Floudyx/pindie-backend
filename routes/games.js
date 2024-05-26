a0bed0c9dede96742bd27b28aa4a288ae74b2b11
Floudyx <ivaan.yudin22@gmail.com>
2024-05-26 20:48:25 +0700

    deploy

---
+++ routes/games.js
@@ -0,0 +1,51 @@
+const gamesRouter = require("express").Router();
+const { checkAuth } = require("../middlewares/auth.js");
+
+const {
+  findAllGames,
+  createGame,
+  findGameById,
+  updateGame,
+  deleteGame,
+  checkEmptyFields,
+  checkIfCategoriesAvaliable,
+  checkIfUsersAreSafe,
+  checkIsGameExists,
+  checkIsVoteRequest,
+} = require("../middlewares/games");
+
+const {
+  sendAllGames,
+  sendGameCreated,
+  sendGameById,
+  sendGameUpdated,
+  sendGameDeleted,
+} = require("../controllers/games");
+
+gamesRouter.get("/games", findAllGames, sendAllGames);
+gamesRouter.get("/games/:id", findGameById, sendGameById);
+gamesRouter.delete("/games/:id", checkAuth, deleteGame, sendGameDeleted);
+gamesRouter.post(
+  "/games",
+  findAllGames,
+  checkIsGameExists,
+  checkIfCategoriesAvaliable,
+  checkEmptyFields,
+  checkAuth,
+  createGame,
+  sendGameCreated
+);
+
+gamesRouter.put(
+  "/games/:id",
+  findGameById,
+  checkIsVoteRequest,
+  checkIfUsersAreSafe,
+  checkIfCategoriesAvaliable,
+  checkEmptyFields,
+  checkAuth,
+  updateGame,
+  sendGameUpdated
+); 
+
+module.exports = gamesRouter;
